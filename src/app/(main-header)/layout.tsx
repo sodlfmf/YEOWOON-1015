9158d0e7f2e8362adb4790508bba27cab9c3825c
hoony0203 <hoony0203@naver.com>
2024-10-23 01:17:09 +0000

    Chore : #1 Add next-sitemap and naver SEO (metatag verfication)

--- src/app/(main-header)/layout.tsx
+++ src/app/(main-header)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 import MainHeader from "@/components/main-header";
 import { Metadata } from "next";
 import Script from "next/script";
-import { Karla,notoSans,fontClassname} from "@/fonts";
+import { Karla, notoSans, fontClassname } from "@/fonts";
 
 export const metadata: Metadata = {
   // meatadataBase 빌드 후 서비스시 변경 필수
@@ -12,6 +12,13 @@ export const metadata: Metadata = {
   title: "YEOWOON | 여운",
   description:
     "We create jewerly that lasts a long time, like a yeowoon, a feeling people can look back on for as long as they want",
+  verification: {
+    google: "",
+    other: {
+      "naver-site-verification":
+        "9f0c7024c0d45b2bce2a98a3520ff41794216ea1",
+    },
+  },
   openGraph: {
     title: "YEOWOON | 여운",
     description:
